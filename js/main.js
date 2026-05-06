a04883ec7cd19194f059c43fefeefc6f6532007d
Pibode <mikailov.03@list.ru>
2026-05-06 09:19:38 +0300

    distribution of basic styles

--- js/main.js
+++ js/main.js
@@ -1,36 +1,63 @@
 
 const body = document.body
 
+
 const myAlert = (text) => {
-    if(typeof text === 'sring'){
+
+        // контейнер
         const container = document.createElement('div')
-        /////
-        //свойства
-        ////
-        
+
+        container.style.boxSizing = 'border-box'
+        container.style.minWidth = '500px'
+        container.style.minHeight = '300px'
+        container.style.padding = '25px'
+
+        container.style.position = 'absolute'
+        container.style.top = '50%'
+        container.style.left = '50%'
+        container.style.transform = 'translate(-50%, -50%)'
+
+        container.style.display = 'flex'
+        container.style.flexDirection = 'column'
+
+
+        container.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.1)'
+        container.style.borderRadius = '5%'
+
+        // заголовок
         const title = document.createElement("h1")
-        title.innerText = "Aleart"
-        /////
-        //свойства
-        ////
+        title.innerText = "Alert"
+        title.style.margin = '0'
         container.append(title)
 
-        const p = document.createElement("p")
-        p.innerText = text
-        /////
-        //свойства
-        ////
-        container.append(p)
+        const divIns = document.createElement("div")
+        divIns.innerText = text
+        divIns.style.fontSize = '20px'
+        divIns.style.width = '420px'
+        divIns.style.minHeight = '100px'
+
+        divIns.style.wordWrap = 'break-word'      
+        divIns.style.whiteSpace = 'normal'       
+        divIns.style.overflowWrap = 'break-word'
+        divIns.style.padding = '25px 0'
+
+
+        container.append(divIns)
 
         const btn = document.createElement("button")
-        /////
-        //свойства
-        ////
+        btn.innerText = 'OK'
+        btn.style.color = 'white'
+        btn.style.background = 'rgba(15, 177, 241, 0.9)'
+        btn.style.borderRadius = '5%'
+        btn.style.border = '0'
+        btn.style.width = '80px'
+        btn.style.height = '40px'
+        btn.style.fontSize = '15px'
+        btn.style.alignSelf = 'flex-end'
         container.append(btn)
 
+
         body.append(container)
-        
-    }else{
-        return
-    }
 }
+
+myAlert(`hello. wold`)
